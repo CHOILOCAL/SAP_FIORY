4c0d96e2a9ebce7a6a880bcd25b250a87c6b749a
HYUNJI CHOI <choihyunji1103@gmail.com>
2024-07-11 06:19:41 +0000

    add a filtering and sorting method

--- unit9_07/webapp/controller/Main.controller.js
+++ unit9_07/webapp/controller/Main.controller.js
@@ -13,6 +13,39 @@ function (Controller) {
             var oEmpModel = new sap.ui.model.json.JSONModel();
             oEmpModel.loadData("/model/employ.json");
             this.getView().setModel(oEmpModel, "emp");
+        },
+
+        onSelectionChange: function (oEvent) {
+            var sLevel = oEvent.getSource().getSelectedKey();
+            // 필터 생성
+            var oFilter = new sap.ui.model.Filter(
+                "Level",
+                sap.ui.model.FilterOperator.EQ,
+                sLevel
+            );
+
+            // 해당 뷰의 UI ID
+            var oTable = this.getView().byId("idTable");
+            oTable.getBinding('rows').filter([oFilter]);
+        },
+
+        onRemoveFilter: function () {
+            var oTable = this.getView().byId("idTable");
+            oTable.getBinding("rows").filter(null);
+            
+            var oComBox = this.getView().byId("comBox");
+            oComBox.setValue("");
+        },
+
+        onSort: function () {
+            var oTable = this.getView().byId("idTable");
+            var oSort = new sap.ui.model.Sorter("Salary", false);
+            oTable.getBinding().sort([oSort]);
+        },
+
+        onRemoveSort: function () {
+            var oTable = this.getView().byId("idTable");
+            oTable.getBinding().sort(null);
         }
     });
 });
